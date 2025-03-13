d78a7bf71c746568934b538624c20ac035f0e3b1
Re9iNee <attarzadeh76@gmail.com>
2025-03-13 14:56:10 +0330

    Homepage and Selected Work content

--- src/app/(Home)/sections/SelectedWork.tsx
+++ src/app/(Home)/sections/SelectedWork.tsx
@@ -4,6 +4,11 @@ import { ClassValue } from "clsx";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
+import cleafin from "@public/Projects/cleafin.png";
+import moraBlog from "@public/Projects/mora-blog.png";
+import moraDash from "@public/Projects/mora-dash.png";
+import aria from "@public/Projects/aria-electric.png";
+
 function SelectedWork({ className }: { className?: ClassValue }) {
   return (
     <section aria-label="selected work" className={cn(className)}>
@@ -12,12 +17,29 @@ function SelectedWork({ className }: { className?: ClassValue }) {
       </h2>
       <div className="space-y-9">
         <Card
-          title="Mora"
+          sampleImage={moraDash}
+          title="Mora Dashboard"
           description="Mora is an online school platform developed to manage students teachers and every aspect of school management"
           image={"/Projects/mora-logo.ico"}
-          imageAlt="Github Icon"
           cta={
             <Link
+              target="_blank"
+              href={"https://demo.mora-ed.com"}
+              className="flex items-center gap-1"
+            >
+              Visit Site <ChevronRight width={20} className="pt-0.5" />
+            </Link>
+          }
+        />
+
+        <Card
+          sampleImage={moraBlog}
+          title="Mora Blog"
+          description="Mora Blog designed to teach people about AI, It's content is based on topics and trends of Artificial Intelligence and how people can adapt and write prompts"
+          image={"/Projects/mora-logo.ico"}
+          cta={
+            <Link
+              target="_blank"
               href={"https://mora-ed.com"}
               className="flex items-center gap-1"
             >
@@ -27,12 +49,13 @@ function SelectedWork({ className }: { className?: ClassValue }) {
         />
 
         <Card
+          sampleImage={cleafin}
           title="Cleafin Marketplace"
           description="MLM Marketplace to manage every aspect of selling online stuff, including digital content or real life products. It has unique features to manage stock, users and ... which supports Multi language websites with different currencies."
           image={"/Projects/cleafin.webp"}
-          imageAlt="Discord Icon"
           cta={
             <Link
+              target="_blank"
               href={"https://cleafin.shop/"}
               className="flex items-center gap-1"
             >
@@ -42,12 +65,13 @@ function SelectedWork({ className }: { className?: ClassValue }) {
         />
 
         <Card
+          sampleImage={aria}
           title="Wordpress Websites"
           description="Worked on so many Wordpress projects. this is the most recent one. This website is designed to showcase electric products on their website."
           image={"/Projects/wordpress.png"}
-          imageAlt="Dribble Icon"
           cta={
             <Link
+              target="_blank"
               href={"https://aria-electric.com/"}
               className="flex items-center gap-1"
             >
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,30 +1,31 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 
-import iPhoneScreenshot from "@public/Projects/Subbi.png";
 import { ReactNode } from "react";
 
 type CardProps = {
   image: string | StaticImport;
-  imageAlt: string;
+
   title: ReactNode;
   description: string;
   cta: ReactNode;
+  sampleImage: StaticImport;
 };
 export default function Card({
   image,
-  imageAlt,
+
   title,
   description,
   cta,
+  sampleImage,
 }: CardProps) {
   return (
-    <div className="group flex w-full justify-between space-y-4 rounded-4xl border border-card-border bg-card-background dark:bg-black/50">
+    <div className="group flex w-full justify-between gap-4 space-y-4 rounded-4xl border border-card-border bg-card-background dark:bg-black/50">
       <div className="flex flex-col justify-between gap-4 px-6 py-8 xl:p-16 xl:pr-0">
         <div className="space-y-4">
           <Image
             src={image}
-            alt={imageAlt}
+            alt={`${title} logo`}
             width={70}
             height={70}
             className="grayscale transition-all group-hover:grayscale-0"
@@ -35,9 +36,10 @@ export default function Card({
         <div className="flex items-center text-18">{cta}</div>
       </div>
       <Image
-        src={iPhoneScreenshot}
-        alt="screenshot of a project"
-        className="hidden xl:block"
+        width={650}
+        src={sampleImage}
+        alt={`screenshot of ${title} project`}
+        className="hidden grayscale transition-all group-hover:grayscale-0 xl:block"
       />
     </div>
   );
